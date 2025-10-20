f22eef024aee9954f68575781c0e9ca6325eacb9
Juan Esteban Moncada Bonilla <juanesmoncada123@gmail.com>
2025-10-20 13:35:39 -0700

    updating cursor rules, deleting windsurf rules, adding MCP go client

--- internal/app/wire.go
+++ internal/app/wire.go
@@ -4,12 +4,27 @@ import (
 	"github.com/modelcontextprotocol/go-sdk/mcp"
 )
 
+const mcpServerInstructions string = "This is a Google Ads MCP server."
+
 func initServer(configs Configs) *mcp.Server {
-	server := mcp.NewServer(&mcp.Implementation{
-		Name:    "Google Ads MCP",
-		Version: "v1.0.0",
-		Title:   "Google Ads MCP server. Use 'system_guidelines' prompt first.",
-	}, nil)
+	implementation := initImplementation()
+	options := getMCPOptions()
+
+	server := mcp.NewServer(implementation, options)
 
 	return server
+}
+
+func initImplementation() *mcp.Implementation {
+	return &mcp.Implementation{
+		Name:    "Google Ads MCP",
+		Version: "v1.0.0",
+		Title:   "Google Ads MCP server.",
+	}
+}
+
+func getMCPOptions() *mcp.ServerOptions {
+	return &mcp.ServerOptions{
+		Instructions: mcpServerInstructions,
+	}
 }
